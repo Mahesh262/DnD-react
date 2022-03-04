dceddc736e9bf958426ae5ed7c73fdf8c6c6f251
Mahesh262 <86904681+Mahesh262@users.noreply.github.com>
2022-03-04 10:21:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,129 @@
+import React, { useState } from 'react';
+import './App.css';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import _ from "lodash";
+import { v4 } from "uuid";
+
+const item = {
+  id: v4(),
+  name: "Clean the house"
+}
+
+const item2 = {
+  id: v4(),
+  name: "Wash the car"
+}
+
+function App() {
+  const [text, setText] = useState("")
+  const [state, setState] = useState({
+    "todo": {
+      title: "Todo",
+      items: [item, item2]
+    },
+    "in-progress": {
+      title: "In Progress",
+      items: []
+    },
+    "done": {
+      title: "Completed",
+      items: []
+    }
+  })
+
+  const handleDragEnd = ({ destination, source }) => {
+    if (!destination) {
+      return
+    }
+
+    if (destination.index === source.index && destination.droppableId === source.droppableId) {
+      return
+    }
+    console.log(handleDragEnd)
+    // Creating a copy of item before removing it from state
+    const itemCopy = { ...state[source.droppableId].items[source.index] }
+    setState(prev => {
+      prev = { ...prev }
+      // Remove from previous items array
+      prev[source.droppableId].items.splice(source.index, 1)
+
+
+      // Adding to new items array location
+      prev[destination.droppableId].items.splice(destination.index, 0, itemCopy)
+
+      return prev
+    })
+  }
+
+  const addItem = () => {
+    setState(prev => {
+      return {
+        ...prev,
+        todo: {
+          title: "Todo",
+          items: [
+            {
+              id: v4(),
+              name: text
+            },
+            ...prev.todo.items
+          ]
+        }
+      }
+    })
+
+    setText("")
+  }
+
+  return (
+    <div className="App">
+      <div style={{ display: 'flex', justifyContent: 'center', flexDirection: "column", marginTop: "120px" }}>
+        <input type="text" className='form-control' value={text} onChange={(e) => setText(e.target.value)} />
+        <button className="btn btn-primary" onClick={addItem}>Add</button>
+      </div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {_.map(state, (data, key) => {
+          return (
+            <div key={key} className={"column"}>
+              <h3>{data.title}</h3>
+              <Droppable droppableId={key}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className={"droppable-col"}
+                    >
+                      {data.items.map((el, index) => {
+                        return (
+                          <Draggable className='bg-success' key={el.id} index={index} draggableId={el.id}>
+                            {(provided, snapshot) => {
+                              console.log(snapshot)
+                              return (
+                                <div
+                                  className={`item ${snapshot.isDragging && "dragging"}`}
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  {el.name}
+                                </div>
+                              )
+                            }}
+                          </Draggable>
+                        )
+                      })}
+                      {provided.placeholder}
+                    </div>
+                  )
+                }}
+              </Droppable>
+            </div>
+          )
+        })}
+      </DragDropContext>
+    </div>
+  );
+}
+
+export default App;
